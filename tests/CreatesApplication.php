16f30fba6354b59f07d08286c38bba815e1e9e5f
charles <c.unsulangi@gmail.com>
2025-10-29 18:24:14 +0700

    Configure development tools: Livewire, Alpine, Larastan, and Pest

---
+++ tests/CreatesApplication.php
@@ -0,0 +1,24 @@
+<?php
+
+// The CreatesApplication trait is not present in the new Laravel 11 structure.
+// We need to create it manually for Pest to work correctly.
+
+namespace Tests;
+
+use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Foundation\Application;
+
+trait CreatesApplication
+{
+    /**
+     * Creates the application.
+     */
+    public function createApplication(): Application
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
+
+        $app->make(Kernel::class)->bootstrap();
+
+        return $app;
+    }
+}
